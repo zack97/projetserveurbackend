6003d1e79fe2e62bad84ce723359589d83d05f49
zack10 <tshibzack@gmail.com>
2024-11-18 01:00:35 +0100

    updating favoris styling

--- public/favoris.php
+++ public/favoris.php
@@ -24,20 +24,49 @@ $favoris = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <title>Mes Favoris</title>
 </head>
 <body>
-    <h2>Mes Articles Favoris</h2>
-    <ul>
-        <?php if (empty($favoris)): ?>
-            <p>Vous n'avez aucun article dans vos favoris.</p>
-        <?php else: ?>
-            <?php foreach ($favoris as $favori): ?>
-                <li>
-                    <h3><?php echo htmlspecialchars($favori['title']); ?></h3>
-                    <p><?php echo htmlspecialchars($favori['content']); ?></p>
-                    <button onclick="supprimerFavori(<?php echo $favori['id']; ?>)">Supprimer des favoris</button>
-                </li>
-            <?php endforeach; ?>
+<header>
+        <img src="/media/fr24_icon.jpg" alt="icon_france24"  >
+        <?php if (isset($_SESSION['user']) && is_array($_SESSION['user']) && isset($_SESSION['user']['first_name'])): ?>
+           <span class="name_display">Welcome, <?php echo htmlspecialchars($_SESSION['user']['first_name']); ?></span> 
+           <hr class="hrhr">
+           <h6 class="h6"><?php echo htmlspecialchars($_SESSION['user']['country']);?></h6>
         <?php endif; ?>
-    </ul>
+        <div class="hamburger none_icon"><i class="fa-solid fa-bars"></i></div>
+        <nav>
+            <a href="/">Accueil</a>
+            <a href="/app/views/apropos.php">À Propos</a>
+            <?php if (isset($_SESSION['user']) && is_array($_SESSION['user']) && isset($_SESSION['user']['first_name'])): ?>
+                <span class="name-drop">Bonjour, <?php echo htmlspecialchars($_SESSION['user']['first_name']); ?></span>
+                <a href="/public/favoris.php" class="logout-button" >Mes Favoris</a>
+                <a href="/logout.php" class="logout-button">Se déconnecter</a>
+            <?php else: ?>
+                <a href="/app/views/user.php">Identifiez-vous</a>
+            <?php endif; ?>
+        </nav>
+
+    </header>
+    <main style="height: 100vh;">
+
+        <h2>Mes Articles Favoris</h2>
+        <ul>
+            <?php if (empty($favoris)): ?>
+                <p>Vous n'avez aucun article dans vos favoris.</p>
+            <?php else: ?>
+                <?php foreach ($favoris as $favori): ?>
+                    <li>
+                        <h3><?php echo htmlspecialchars($favori['title']); ?></h3>
+                        <p><?php echo htmlspecialchars($favori['content']); ?></p>
+                        <button onclick="supprimerFavori(<?php echo $favori['id']; ?>)">Supprimer des favoris</button>
+                    </li>
+                <?php endforeach; ?>
+            <?php endif; ?>
+        </ul>
+    </main>
+   
+
+    <footer>
+        <p>© 2024 France24. Tous droits réservés.</p>
+    </footer>
 
     <script>
         function supprimerFavori(articleId) {
